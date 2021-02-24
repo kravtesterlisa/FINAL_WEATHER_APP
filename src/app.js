31b9c55c3dcc474169edeaae4656f44ffcfc4ec5
kravtesterlisa <kravtesterlisa@gmail.com>
2021-02-25 00:17:41 +0300

    add time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatData (timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours <10){
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes <10){
+        minutes = `0${minutes}`;
+    }
+    let week_days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    let day = week_days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+
+
+
+
+
+}
+
+
+
+
 
 function displayTemperature (response) {
     console.log(response.data);
@@ -6,11 +29,13 @@ function displayTemperature (response) {
     let weather_discription = document.querySelector("#weather_description");
     let humidity_value = document.querySelector("#humidity_value");
     let wind_speed = document.querySelector("#wind_speed");
+    let current_date = document.querySelector("#date_time");
     temperature_view.innerHTML = Math.round(response.data.main.temp);
     city_view.innerHTML = response.data.name;
     weather_discription.innerHTML = response.data.weather[0].description;
     humidity_value.innerHTML = response.data.main.humidity;
     wind_speed.innerHTML = Math.round(response.data.wind.speed);
+    current_date.innerHTML = formatData(response.data.dt * 1000);
 
 
     
